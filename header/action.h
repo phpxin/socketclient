d9502b9e02df8817d5a3e1324942961ff4c7e222
lixin65535@126.com <lixin65535@126.com>
2016-10-26 15:33:32 +0800

    #

--- header/action.h
+++ header/action.h
@@ -1,8 +1,21 @@
+#define CHAR_SIZE_1 200
+#define TEXT_SIZE_1 65535
+
 struct user{
 	int id;
-	char account[200];
-	char pwd[200];
-	char name[200];
+	char account[CHAR_SIZE_1];
+	char pwd[CHAR_SIZE_1];
+	char name[CHAR_SIZE_1];
+	char avatar[CHAR_SIZE_1];
+};
+
+struct message{
+	int id;
+	int uid;
+	int fid;
+	char content[TEXT_SIZE_1];
+	short type;
+    time_t addtime;
 };
 
 void act_login();
